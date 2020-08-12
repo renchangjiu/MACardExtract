c68d97f89c40bf82ac34cb272a4c2019d139c203
sumuzhe <1359581862@qq.com>
2020-08-12 20:53:47 +0800

    commit

--- app.py
+++ app.py
@@ -13,6 +13,12 @@ class App(object):
         super().__init__()
         self.success_count = 0
         self.failed_count = 0
+        self.c404s = []
+        self.c404_path = Config.save_path + "/404.txt"
+        if os.path.exists(self.c404_path):
+            f = open(self.c404_path, "r")
+            self.c404s = list(map(lambda x: x.strip(), f.readlines()))
+            f.close()
 
     def run(self):
         file = open("card.csv", mode="r", encoding="utf-8")
@@ -28,13 +34,23 @@ class App(object):
     def __request(self, card_id: str, card_name: str):
         card_name = card_name.replace("/", "")
         path = App.fmt_save % (Config.save_path, card_id, card_name)
+        url = App.fmt_url % card_id
+
+        # 跳过已下载的立绘
         if os.path.exists(path):
             self.success_count += 1
             print(path + "\t exists, break")
             print("total: %d, success: %d, failed: %d" % (
                 self.success_count + self.failed_count, self.success_count, self.failed_count))
             return
-        url = App.fmt_url % card_id
+
+        # 跳过404的立绘
+        if card_id in self.c404s:
+            self.failed_count += 1
+            print("404: " + url)
+            print("total: %d, success: %d, failed: %d" % (
+                self.success_count + self.failed_count, self.success_count, self.failed_count))
+            return
         r = requests.get(url)
         code = r.status_code
         if code != 404:
@@ -45,8 +61,16 @@ class App(object):
         else:
             self.failed_count += 1
             print("404: " + url)
+            self.__write404s(card_id)
         print("total: %d, success: %d, failed: %d" % (
             self.success_count + self.failed_count, self.success_count, self.failed_count))
+
+    def __write404s(self, card_id: str):
+        self.c404s.append(card_id)
+        copy = list(map(lambda x: x + "\n", self.c404s.copy()))
+        f = open(self.c404_path, "w")
+        f.writelines(copy)
+        f.close()
 
 
 if __name__ == "__main__":
